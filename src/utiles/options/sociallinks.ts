539abb5b13ba78c341b19c2e94f7c60bbe48d534
Ahmad <info.websphereuk.com>
2024-06-30 20:42:34 +0500

    dw

--- src/utiles/options/sociallinks.ts
+++ src/utiles/options/sociallinks.ts
@@ -2,6 +2,6 @@ export const socialLinks = {
     Facebook: 'https://www.facebook.com/people/Websphereuk/100084813102149/?mibextid=rS40aB7S9Ucbxw6v',
     Linkedin: 'https://www.linkedin.com/in/websphere-uk',
     Youtube: 'https://www.youtube.com/channel/UC9K72SBGxrtYeBtbtakdj_A',
-    Instagram: 'https://www.instagram.com/p/C8tDFZpII3m/'
+    Instagram: 'https://www.instagram.com/websphereuk/'
 
 }
