bba7ba2f676dc75fcc72bd0d6fb9ef8a48daa4f2
Ahmad <info.websphereuk.com>
2024-07-11 14:17:17 +0500

    Check Status

--- src/utiles/options/patnerships-models-data.ts
+++ src/utiles/options/patnerships-models-data.ts
@@ -4,21 +4,21 @@ export const PartnershipsModelsData = [
         Heading: "Static Price Structure",
         Paragraph: "In this approach, project parameters such as scope, cost, and timeline are clearly defined from the outset, making it particularly well-suited for extended commitments. Clients benefit from budget predictability, making it an ideal choice for those with precise requirements",
         ButtonText: "Schedule A Call",
-        ButtonLink: "/contact"
+        ButtonLink: "/contact-us"
     },
     {
         Id: "2",
         Heading: "Employ Exclusive Talent Model",
         Paragraph: "In this streamlined model, clients pay for developer hours, ensuring a seamless project initiation and effortless feature updates. The precision in payment transparency not only fosters trust but also enhances communication throughout the process, greather  collaboration ",
         ButtonText: "Schedule A Call",
-        ButtonLink: "/contact"
+        ButtonLink: "/contact-us"
     },
     {
         Id: "3",
         Heading: " On-location Development Approach",
         Paragraph: " Clients often prefer to adopt this model to receive comprehensive on-site development support. It is widely regarded as a cost-effective and efficient choice that not only encourages valuable face-to-face interaction but also fosters strong team dynamics",
         ButtonText: "Schedule A Call",
-        ButtonLink: "/contact"
+        ButtonLink: "/contact-us"
     },
 
 ]
